7e42edd304c7fe809276ec80eace868198a81a83
Nakakousei <e17131@numazu.kosen-ac.jp>
2020-07-13 16:53:49 +0900

    click program

---
+++ click.js
@@ -0,0 +1,15 @@
+
+(function() {
+
+    function init() {
+        window.addEventListener("click",function(event){
+            let content =  document.getElementById("content");
+            let cX = event.pageX;
+            let cY = event.pageY;
+            let lect = content.getBoundingClientRect();
+            l
+        });
+    }
+
+    window.addEventListener("load", init, false);
+})();
